74f6c909094291b3a21540570845c1a8c7705bee
Paula <paula@paulas-air.watson.ibm.com>
2023-06-12 08:38:45 -0400

    Add data script

--- SOMOSPIE_pipeline/data/data.py
+++ SOMOSPIE_pipeline/data/data.py
@@ -12,27 +12,40 @@ from sklearn.datasets import load_breast_cancer
 def get_parser():
     parser = argparse.ArgumentParser(description='Arguments and data files for executing Nearest Neighbors Regression.')
     parser.add_argument('-t', "--trainingdata", help='Training data')
-    #parser.add_argument('-m', "--pathtomodel", help='Directory where the knn model will be saved')
+    parser.add_argument('-m', "--pathtomodel", help='Directory where the knn model will be saved')
     return parser
 
 def load_data(args):
     print("Reading training data from", args.trainingdata)
-        # Gets and split dataset
-    x, y = load_breast_cancer(return_X_y=True)
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+    training_data = pd.read_csv(args.trainingdata+"train.csv")
+    col = list(training_data.columns)
+    col[2] = 'z'
+    training_data.columns = col
+    pathtomodel = args.trainingdata
+    # Create path to where data and scaler is saved
+    Path(pathtomodel).parent.mkdir(parents=True, exist_ok=True)
+    
+    x_train, x_test, y_train, y_test = train_test_split(training_data.loc[:,training_data.columns != 'z'], training_data.loc[:,'z'], test_size=0.1)
+    #print(x_train,"\n",y_train,"TEST\n",x_test,y_test)
+    ss = StandardScaler()
+    x_train = ss.fit_transform(x_train)
+    x_test = ss.transform(x_test)
+    #print("SCALED")
+    #print(x_train,"\n",y_train,"TEST\n",x_test,y_test)
 
-    # Creates `data` structure to save and 
-    # share train and test datasets.
+    # Save scaler model so it can be reused for predicting
+    pickle.dump(ss, open(pathtomodel+'scaler.pkl', 'wb'))
+
+    # Save data to train with different ml-models
     data = {'x_train' : x_train.tolist(),
             'y_train' : y_train.tolist(),
             'x_test' : x_test.tolist(),
             'y_test' : y_test.tolist()}
-
     # Creates a json object based on `data`
     data_json = json.dumps(data)
 
     # Saves the json object into a file
-    with open(args.trainingdata, 'w') as out_file:
+    with open(pathtomodel, 'w') as out_file:
         json.dump(data_json, out_file)
 
 if __name__ == "__main__":
