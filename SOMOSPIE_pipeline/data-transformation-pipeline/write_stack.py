15b9b6d7b7ce2f9dbb1a6354999861fdca7f08c4
Paula <paula@paulas-air.watson.ibm.com>
2023-07-24 11:17:06 -0400

    Integrate functions in bigger files

--- SOMOSPIE_pipeline/data-transformation-pipeline/write_stack.py
+++
@@ -1,6 +0,0 @@
-def write_stack(vrt_file:str, out_file:str)->str:
-    from osgeo import gdal
-    
-    translate_options = gdal.TranslateOptions(creationOptions=['COMPRESS=LZW', 'TILED=YES', 'BIGTIFF=YES'], callback=gdal.TermProgress_nocb)
-    gdal.Translate(out_file, vrt_file, options=translate_options)
-    return out_file
