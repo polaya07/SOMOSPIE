3e8d0a491bae8882b27fad40cefcd8d95344d55e
Paula <paula@Paulas-MacBook-Air.local>
2023-07-27 17:02:25 -0400

    Update ml pipeline

--- SOMOSPIE_pipeline/ml-model-pipeline/pipeline.py
+++ SOMOSPIE_pipeline/ml-model-pipeline/pipeline.py
@@ -6,9 +6,8 @@ import pandas as pd
 # Import functions from other files
 from load_data import *
 from knn_train import *
-from knn_inference import *
 from rf_train import *
-from rf_inference import *
+from inference import *
 #from tif2df import *
 
 @dsl.pipeline(name='somospiepipeline', description='Pipeline for somospie')
@@ -66,17 +65,17 @@ def pipeline(
     # How to define a component: https://kubeflow-pipelines.readthedocs.io/en/stable/source/kfp.components.html#kfp.components.create_component_from_func
     data_op = kfp.components.create_component_from_func(load_data, base_image = str(container_image))#packages_to_install=["numpy", "pandas", "scikit-learn"])
     knn_train_op = kfp.components.create_component_from_func(knn_train,  base_image = str(container_image))#packages_to_install=["numpy", "pandas", "scikit-learn"])
-    knn_inference_op = kfp.components.create_component_from_func(knn_inference,  base_image = str(container_image))#packages_to_install=["numpy", "pandas", "scikit-learn"])
     rf_train_op = kfp.components.create_component_from_func(rf_train,  base_image =str(container_image))#packages_to_install=["numpy", "pandas", "scikit-learn"])
-    rf_inference_op = kfp.components.create_component_from_func(rf_inference,  base_image = str(container_image))#packages_to_install=["numpy", "pandas", "scikit-learn"])
+    inference_op = kfp.components.create_component_from_func(inference,  base_image = str(container_image))#packages_to_install=["numpy", "pandas", "scikit-learn"])
     #tif2df_op = kfp.components.create_component_from_func(tif2df,  base_image = str(container_image))#packages_to_install=["numpy", "pandas", "scikit-learn"])
 
+    region_res: str = "tn-30m"
     year = 2010
     months = 3
     
     for month in range(1,months+1):
-        train_file = "/train/"+'{0:04d}_{1:02d}.tif'.format(year, month + 1)
-        temp_data_json = "/train/"+'{0:04d}_{1:02d}.json'.format(year, month + 1)
+        train_file = "/train/"+'{0:04d}_{1:02d}.tif'.format(year, month)
+        temp_data_json = "/train/"+'{0:04d}_{1:02d}.json'.format(year, month)
         # Get data and split in train and validation
         data_task = data_op(train_file, "/train/", temp_data_json).add_pvolumes({"/train/": pvc_train.volume}).set_retry(3)
         # Train after splitting the data
@@ -90,15 +89,15 @@ def pipeline(
         # Inference on multiple tiles
         for i in range(total_tiles):
             #tile_id = "midwest_region_evaluation_10m_grid_{0:03d}".format(i) ## Check format to read
-            tile_id = "eval_tile_{0:04d}".format(i)
+            tile_id = region_res+"_eval_{0:04d}".format(i)
             ## Read Data:
             #tif2df_task.append(tif2df_op("/eval/"+tile_id+".csv", band_names))
             ## KNN:
-            knn_inference_task = knn_inference_op(knn_train_task.output, data_task.outputs['scaler'], "/eval/"+tile_id+".tif" , "/out_knn/knn/",
+            knn_inference_task = inference_op(knn_train_task.output, data_task.outputs['scaler'], "/eval/"+tile_id+".tif" , "/out_knn/knn/",
                                 "/out_knn/knn/"+tile_id+".tif","/out_knn/knn/"+tile_id+".csv").add_pvolumes({"/train/": pvc_train.volume, "/eval/": pvc_eval.volume, 
                                 "/out_knn/": pvc_predictions.volume}).set_retry(3).set_memory_request('20G').add_node_selector_constraint("ibm-cloud.kubernetes.io/worker-pool-name", "inference")
             ## RF:
-            rf_inference_task = rf_inference_op(rf_train_task.output, data_task.outputs['scaler'],  "/eval/"+tile_id+".tif" , "/out_rf/rf/",
+            rf_inference_task = inference_op(rf_train_task.output, data_task.outputs['scaler'],  "/eval/"+tile_id+".tif" , "/out_rf/rf/",
                                 "/out_rf/rf/"+tile_id+".tif", "/out_rf/rf/"+tile_id+".csv").add_pvolumes({"/train/": pvc_train.volume, "/eval/": pvc_eval.volume, 
                                 "/out_rf/": pvc_predictions.volume}).set_retry(3).set_memory_request('20G').add_node_selector_constraint("ibm-cloud.kubernetes.io/worker-pool-name", "inference")
 
